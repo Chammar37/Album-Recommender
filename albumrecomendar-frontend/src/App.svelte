3d4bd1496809701c368306ff708340aa8a7028b4
Chammar37 <m.chami37@hotmail.com>
2023-09-22 15:31:07 -0400

    changes

--- albumrecomendar-frontend/src/App.svelte
+++ albumrecomendar-frontend/src/App.svelte
@@ -11,14 +11,16 @@
         const response = await fetch('http://localhost:8080/recommendations?seedArtist=2ANVost0y2y52ema1E9xAZ&seedGenres=hip-hop&seedTracks=0c6xIDDpzE81m2q797ordA&targetEnergy=0.7&targetDanceability=0.7&targetValence=0.1');
         albums = await response.json();
         // console.log(response)
-        console.log(albums)
+        console.log("on mount display " + albums)
     });
 
     const handleAlbumClick = async event => {
-        const album = event.detail;
+        const album = event;
+        console.log("album clicked info " + album)
+        console.log("album clicked name " + album.name)
         const response = await fetch(`http://localhost:8080/search?query=${album.name}`)
         albums = await response.json();
-        console.log("similar albums" + similarAlbums)
+        console.log("album search" + albums)
         // similarAlbums = albums || [];
           
     }
@@ -36,13 +38,12 @@
             <Album {...album} on:albumselect={handleAlbumClick} on:keydown="{handleKeyDown}" />
         {/each}
     
-        {#if similarAlbums}
-        
+        {#if similarAlbums}        
             <SimilarAlbums {similarAlbums} />
         {/if}
 
-    {:else}
-    <p>Loading album ...</p>
+        {:else}
+            <p>Loading album ...</p>
     {/if}
 </div>
 
